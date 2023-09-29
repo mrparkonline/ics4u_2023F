9677dd5c7a8a49f978fa0f4c963404323130ed10
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-28 21:13:53 -0400

    added a working integer input

--- video_solution/vid18.py
+++ video_solution/vid18.py
@@ -7,5 +7,8 @@ Given an integer input greater than 0 (Assume that your input is greater than 0)
 # Solution Brainstorm
 # 1. Get an integer as user input
 # 2. Need to generate all numbers from 1 upto user_input
-# 3. Check if any numbers generated are factors (if num is divisible by the generator)
+# 3. Check if any numbers generated are factors (if num is divisible by the generated numbers, we have a factor)
 # 4. if true... print the number out
+
+# 1. Get an integer as user input
+num = int(input("Enter an integer greater than 0: "))
