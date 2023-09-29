699ae97f29e59c038b080ca2082ecf93b68f28aa
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-28 20:07:59 -0400

    Initial Solution of vid17.py

---
+++ video_solution/vid17.py
@@ -0,0 +1,31 @@
+# Factorial Question Solution
+
+# Problem input:
+num = int(input("Enter a value of N: "))
+
+# Solution 1 --> While Loop
+total_product = 1
+multiplier = 1
+
+while multiplier <= num:
+    # We indent for start of while loop code block
+    # this solutions goes from [1 to N]
+    total_product = total_product * multiplier;
+    # faster way --> total-product
+
+    multiplier = multiplier + 1
+    # multiplier += 1
+# end of while loop ... # we unindented to end the while loop's code block
+print(f"Factorial of {num} is {total_product}.")
+
+# Solution 2 --> For Loop
+# This solutions goes from [N to 0)
+
+answer2 = 1
+
+for i in range(num, 0, -1):
+    answer2 *= num
+# end of for loop
+
+print(f"Factorial of {num} is {answer2}.")
+
