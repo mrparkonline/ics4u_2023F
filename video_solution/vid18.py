dc264311f2b8b04f98e3ec171c5620c33e5ef1bb
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-28 21:30:40 -0400

    completed the solution

--- video_solution/vid18.py
+++ video_solution/vid18.py
@@ -16,3 +16,9 @@ num = int(input("Enter an integer greater than 0: "))
 # 2. Need to generate all numbers from 1 upto user_input
 for divider in range(1, num+1):
     print(f"divider variable is: {divider}.")
+
+    # 3. Check if any numbers generated are factors (if num is divisible by the generated numbers, we have a factor)
+    if num % divider == 0:
+        # we have a factor!
+        print(f"{divider} is a factor for {num}.")
+        # 4. if true... print the number out
