f4c1f74822a249bb52e5251c056ce244e4316bdb
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 15:43:12 -0400

    solution planned

--- video_solution/vid21.py
+++ video_solution/vid21.py
@@ -1,3 +1,11 @@
 # The Most Number of Factors (Solution Link)
 # From 1 to N, where N is an integer greater one. We will output the number between [1,N] that has the most factors. 
 # Example: From numbers 1 to 5. → The number 4 has the most factors.
+
+# Solution Planning
+#   1. Take N is input
+#   2. Set max_count to 0, set the result_num to 0 ... variable initialization
+#   3. Generate numbers from [1 to N]
+#   4. count the number of factors if each numbers generated
+#   5. if the new count is larger than our max_count, update max_count and result_num
+
