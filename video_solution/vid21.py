1dc18f3cf25a002c730995367f18089677dd2862
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 15:40:27 -0400

    vid21 initialized

---
+++ video_solution/vid21.py
@@ -0,0 +1,3 @@
+# The Most Number of Factors (Solution Link)
+# From 1 to N, where N is an integer greater one. We will output the number between [1,N] that has the most factors. 
+# Example: From numbers 1 to 5. → The number 4 has the most factors.
