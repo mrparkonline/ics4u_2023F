360e74b89665bbf2dc55de323a4aedac225ada4e
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 15:07:26 -0400

    vid20 initialized

---
+++ video_solution/vid20.py
@@ -0,0 +1,9 @@
+# Q4) Perfect Numbers (Solution Link)
+ 
+# The proper divisors of a number are the factors of a number that is not equal to itself.
+
+# Example → 12 has the proper divisors of: 1, 2, 3, 4, 6.
+
+# A perfect number is defined when the sum of a number’s proper divisors equal to the number itself.
+
+# Find the total sum of perfect numbers under 10,000.
