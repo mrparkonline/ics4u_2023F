16b30223ae9588b1297e3491f6f7dbcea3c875fb
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 17:23:16 -0400

    vid23 initialized

---
+++ video_solution/vid23.py
@@ -0,0 +1,4 @@
+# Q7) Average Calculator (Solution Link)
+# Create a program that calculates the averages of the marks that user inputs. 
+# The user is allowed to input any amount of marks they want as long as it is 0 to 100.
+
