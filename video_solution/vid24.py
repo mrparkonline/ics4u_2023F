7b73bc7245eb9bf2dcd8fa1f845034d07f8d85d4
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 17:51:35 -0400

    vid24 initialized

---
+++ video_solution/vid24.py
@@ -0,0 +1,3 @@
+# Longest Name. (Solution Link)
+# Create a program that takes an unknown amount of names and outputs the longest name. You will know that the inputs are done when the user has inputted a string of X
+
