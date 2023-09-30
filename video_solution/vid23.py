98d0d3821fb2f60b82649625a9c8ace0ff6aa168
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 17:25:14 -0400

    solution planned

--- video_solution/vid23.py
+++ video_solution/vid23.py
@@ -2,3 +2,9 @@
 # Create a program that calculates the averages of the marks that user inputs. 
 # The user is allowed to input any amount of marks they want as long as it is 0 to 100.
 
+# Solution Plan
+
+# 1. Create an infinite loop with a variable set to True
+# 2. Set the variable false when user input is "exit"
+# 3. add valid marks to a variable, count the number of valid marks
+# 4. After exiting the loop when possible, then calculate the average
