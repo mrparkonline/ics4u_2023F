ff8044c94bd9d77bd67d2a02687a5de1bf24be0b
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 16:23:10 -0400

    vid22 initialized

---
+++ video_solution/vid22.py
@@ -0,0 +1,4 @@
+# Q6) Create an Nth Fibonacci number finder (Solution Link)
+# Given an integer input of N greater than -1, create a program that outputs the Nth fibonacci number.
+# Example.
+# 0th Fibonacci number is 0, 1st Fibonnaci number is 1,  10th is 55, 19th 4181
