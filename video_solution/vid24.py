3200fdbbc98920d402546da6fdd2236894a0aebe
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 17:53:05 -0400

    solution planned

--- video_solution/vid24.py
+++ video_solution/vid24.py
@@ -1,3 +1,9 @@
 # Longest Name. (Solution Link)
 # Create a program that takes an unknown amount of names and outputs the longest name. You will know that the inputs are done when the user has inputted a string of X
 
+# Solution plan
+
+# 1. Different kind of infinite loop, condition: while name != 'X'
+# 2. initialize, name that creates the longest length, the longest length value
+# 3. analyze each input, grab the length via len(), compare and update if needed
+
