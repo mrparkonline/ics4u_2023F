3e473c427c399caabbd90e00254e549719383663
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 18:19:14 -0400

    solution finished

--- video_solution/vid25.py
+++ video_solution/vid25.py
@@ -4,3 +4,17 @@
 # 10 and 24 shares:
 # 1
 # 2
+
+# Solution
+# the largest factor that two numbers (a,b) can share is the minimum of a and b
+# generate numbers from [1 to min(a,b)] then find factors to output
+
+num1 = int(input('Enter num1: '))
+num2 = int(input('Enter num2: '))
+
+upper_limit = min(num1, num2)
+
+for divider in range(1, upper_limit+1):
+    if num1 % divider == 0 and num2 % divider == 0:
+        print(f'{num1} and {num2} share a factor of {divider}.')
+# end of for loop
