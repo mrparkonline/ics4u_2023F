628b5a28347f0cc55eca9f63b2aa9217cab7a1e5
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 16:20:43 -0400

    solution finished

--- video_solution/vid21.py
+++ video_solution/vid21.py
@@ -6,6 +6,26 @@
 #   1. Take N is input
 #   2. Set max_count to 0, set the result_num to 0 ... variable initialization
 #   3. Generate numbers from [1 to N]
-#   4. count the number of factors if each numbers generated
+#   4. count the number of factors of if each numbers generated
 #   5. if the new count is larger than our max_count, update max_count and result_num
 
+upper_limit = int(input("Enter N: "))
+
+max_count = 0
+result_num = 0
+
+for num in range(1, upper_limit+1):
+    total_factors = 0 # reset this variable everytime this for loop executes the code block
+
+    # counting the number of factors for num
+    for divider in range(1, num+1):
+        if num % divider == 0:
+            total_factors += 1
+    # end of inner for loop
+
+    if total_factors > max_count:
+        max_count = total_factors # update to hold the largest count so far
+        result_num = num # update to hold num, which generated the most factors so far
+# end of outer for loop
+
+print(f"{result_num} had the most factors, with {max_count} factors.")
