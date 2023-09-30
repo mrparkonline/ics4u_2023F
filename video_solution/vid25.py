e49e9203c33de76958d9bae1cf6a7fe69833be0e
Mr. Park (Windows) <mrparkonline@gmail.com>
2023-09-30 18:06:10 -0400

    vid25 initialized

---
+++ video_solution/vid25.py
@@ -0,0 +1,6 @@
+# Q9) Common Factors (Solution Link)
+# When given two positive integers greater than 1 which are A and B, write a program that prints all the factors that they both share.
+
+# 10 and 24 shares:
+# 1
+# 2
